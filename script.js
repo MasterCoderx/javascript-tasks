a21f787f1f22eecf5e97f75092d558e9799a0e24
Jan_Jagoda <jan.jagoda@outlook.com>
2023-07-25 23:16:47 +0200

    Completed Exercise 3

--- script.js
+++ script.js
@@ -2,16 +2,31 @@
 
 const greeting = getGreeting('John', 'Smith');
 function getGreeting(firstName, secondName) {
-    return 'Hi, "' + firstName + ' ' + secondName + '". What\'s up?'
+    return 'Hi, "' + firstName + ' ' + secondName + '". What\'s up?';
 }
 console.log(greeting); // Hi, “John Smith”. What's up?
 
 // Exercise 2 //
 
-function isThisMyName(x) {
-    return x === 'Janek'
+function isThisMyName(yourName) {
+    return yourName === 'Janek';
 }
 
-console.log(isThisMyName('Janek'))
-console.log(isThisMyName('Adam'))
+console.log(isThisMyName('Janek'));
+console.log(isThisMyName('Adam'));
 
+// Exercise 3 //
+
+function isThisBoolean (x) {
+    if (x === true) {
+        return true
+    }
+    else if (x === false) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+console.log(isThisBoolean('false'))

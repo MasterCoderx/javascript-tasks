18ced6dfdd9fa5206ce2f8bf0fbbf1a78e70a606
Jan_Jagoda <jan.jagoda@outlook.com>
2023-07-25 22:21:07 +0200

    Completed Excercise 1

---
+++ script.js
@@ -0,0 +1,8 @@
+//  Exercise 1 //
+
+const greeting = getGreeting('John', 'Smith');
+function getGreeting(firstName, secondName) {
+    return 'Hi, "' + firstName + secondName + '". What\'s up?'
+}
+console.log(greeting); // Hi, “John Smith”. What's up?
+
